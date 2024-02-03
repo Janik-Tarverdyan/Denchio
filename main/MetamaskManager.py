b7a43e907b7878ffab2ecaa025f06c2693cf8199
Janik Tarverdyan <Janik.Tarverdyan@gmail.com>
2024-02-03 16:47:08 +0400

    Update: change price, set hardcode method

--- main/MetamaskManager.py
+++ main/MetamaskManager.py
@@ -49,7 +49,7 @@ class MetamaskManager:
                     price = round(price_dench_usd / price_base_usd, 3)
         except Exception as e:
             print(e)
-            price = 0.0
+            price = 10.353
         return price
 
     @staticmethod
